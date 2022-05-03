35e9aa1fb29fc9b8ab0bca229527357b259b50a9
Brandon Lum <lumjjb@gmail.com>
2022-05-03 15:52:44 -0400

    something about custom predicates
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

---
+++ cmd/intoto-custom-predicate/attest.go
@@ -0,0 +1,185 @@
+// Copyright 2022 SLSA Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strings"
+
+	intoto "github.com/in-toto/in-toto-golang/in_toto"
+	slsav02 "github.com/in-toto/in-toto-golang/in_toto/slsa_provenance/v0.2"
+	"github.com/spf13/cobra"
+
+	"github.com/slsa-framework/slsa-github-generator/signing/sigstore"
+)
+
+var (
+	// shaCheck verifies a hash is has only hexidecimal digits and is 64
+	// characters long.
+	shaCheck = regexp.MustCompile(`^[a-fA-F0-9]{64}$`)
+
+	// wsSplit is used to split lines in the subjects input.
+	wsSplit = regexp.MustCompile(`[\t ]`)
+)
+
+// parseSubjects parses the value given to the subjects option.
+func parseSubjects(subjectsStr string) ([]intoto.Subject, error) {
+	var parsed []intoto.Subject
+
+	scanner := bufio.NewScanner(strings.NewReader(subjectsStr))
+	for scanner.Scan() {
+		// Split by whitespace, and get values.
+		parts := wsSplit.Split(strings.TrimSpace(scanner.Text()), 2)
+
+		// Lowercase the sha digest to comply with the SLSA spec.
+		shaDigest := strings.ToLower(strings.TrimSpace(parts[0]))
+		if shaDigest == "" {
+			// Ignore empty lines.
+			continue
+		}
+		// Do a sanity check on the SHA to make sure it's a proper hex digest.
+		if !shaCheck.MatchString(shaDigest) {
+			return nil, fmt.Errorf("unexpected sha256 hash %q", shaDigest)
+		}
+
+		// Check for the subject name.
+		if len(parts) == 1 {
+			return nil, fmt.Errorf("expected subject name for hash %q", shaDigest)
+		}
+		name := strings.TrimSpace(parts[1])
+
+		for _, p := range parsed {
+			if p.Name == name {
+				return nil, fmt.Errorf("duplicate subject: %q", name)
+			}
+		}
+
+		parsed = append(parsed, intoto.Subject{
+			Name: name,
+			Digest: slsav02.DigestSet{
+				"sha256": shaDigest,
+			},
+		})
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return parsed, nil
+}
+
+func getFile(path string) (io.Writer, error) {
+	if path == "-" {
+		return os.Stdout, nil
+	}
+	return os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0600)
+}
+
+// attestCmd returns the 'attest' command.
+func attestCmd() *cobra.Command {
+	var (
+		attPath       string
+		subjects      string
+		predicateFile string
+		predicateType string
+	)
+
+	c := &cobra.Command{
+		Use:   "attest",
+		Short: "Create a signed SLSA attestation from a Github Action",
+		Long: `Generate and sign SLSA provenance from a Github Action to form an attestation
+and upload to a Rekor transparency log. This command assumes that it is being
+run in the context of a Github Actions workflow.`,
+
+		Run: func(cmd *cobra.Command, args []string) {
+			/*
+				ghContext, err := github.GetWorkflowContext()
+				check(err)
+			*/
+
+			parsedSubjects, err := parseSubjects(subjects)
+			check(err)
+
+			// TODO: read predicates file os.ReadFile
+			predicateBytes, err := os.ReadFile(predicateFile)
+			check(err)
+
+			var genericPredicate map[string]interface{}
+			err = json.Unmarshal(predicateBytes, &genericPredicate)
+			check(err)
+
+			if len(parsedSubjects) == 0 {
+				check(errors.New("expected at least one subject"))
+			}
+
+			ctx := context.Background()
+
+			/*
+				c, err := github.NewOIDCClient()
+				check(err)
+
+				// TODO: Change to intoto
+				p, err := slsa.HostedActionsProvenance(ctx, slsa.NewWorkflowRun(parsedSubjects, ghContext), c)
+				check(err)
+			*/
+
+			p, err := CustomIntotoStatement(parsedSubjects, predicateType, genericPredicate)
+			check(err)
+
+			fmt.Println(p)
+
+			if attPath != "" {
+				s := sigstore.NewDefaultSigner()
+				att, err := s.Sign(ctx, p)
+				check(err)
+
+				_, err = s.Upload(ctx, att)
+				check(err)
+
+				f, err := getFile(attPath)
+				check(err)
+
+				_, err = f.Write(att.Bytes())
+				check(err)
+			}
+		},
+	}
+
+	c.Flags().StringVarP(&attPath, "signature", "g", "attestation.intoto.jsonl", "Path to write the signed attestation.")
+	c.Flags().StringVarP(&subjects, "subjects", "s", "", "Formatted list of subjects in the same format as sha256sum.")
+	c.Flags().StringVarP(&predicateType, "predicateType", "t", "", "Predicate type for intoto statement header.")
+	c.Flags().StringVarP(&predicateFile, "predicateFile", "f", "predicate.json", "Path to retrieve custom predicate to create attestation from")
+
+	return c
+}
+
+// CustomIntotoStatement creates an intoto statement with provided fields
+func CustomIntotoStatement(subjects []intoto.Subject, predicateType string, predicate interface{}) (*intoto.Statement, error) {
+	return &intoto.Statement{
+		StatementHeader: intoto.StatementHeader{
+			Type:          intoto.StatementInTotoV01,
+			PredicateType: predicateType,
+			Subject:       subjects,
+		},
+		Predicate: predicate,
+	}, nil
+}
---
+++ cmd/intoto-custom-predicate/attest_test.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+
+	intoto "github.com/in-toto/in-toto-golang/in_toto"
+	slsav02 "github.com/in-toto/in-toto-golang/in_toto/slsa_provenance/v0.2"
+)
+
+// TestParseSubjects tests the parseSubjects function.
+func TestParseSubjects(t *testing.T) {
+	testCases := []struct {
+		name     string
+		str      string
+		expected []intoto.Subject
+		err      bool
+	}{
+		{
+			name: "single",
+			str:  "2e0390eb024a52963db7b95e84a9c2b12c004054a7bad9a97ec0c7c89d4681d2 hoge",
+			expected: []intoto.Subject{
+				{
+					Name: "hoge",
+					Digest: slsav02.DigestSet{
+						"sha256": "2e0390eb024a52963db7b95e84a9c2b12c004054a7bad9a97ec0c7c89d4681d2",
+					},
+				},
+			},
+		},
+		{
+			name: "name has spaces",
+			str:  "2e0390eb024a52963db7b95e84a9c2b12c004054a7bad9a97ec0c7c89d4681d2 hoge fuga",
+			expected: []intoto.Subject{
+				{
+					Name: "hoge fuga",
+					Digest: slsav02.DigestSet{
+						"sha256": "2e0390eb024a52963db7b95e84a9c2b12c004054a7bad9a97ec0c7c89d4681d2",
+					},
+				},
+			},
+		},
+		{
+			name: "extra whitespace",
+			str:  "\t  2e0390eb024a52963db7b95e84a9c2b12c004054a7bad9a97ec0c7c89d4681d2 \t hoge fuga  \t  ",
+			expected: []intoto.Subject{
+				{
+					Name: "hoge fuga",
+					Digest: slsav02.DigestSet{
+						"sha256": "2e0390eb024a52963db7b95e84a9c2b12c004054a7bad9a97ec0c7c89d4681d2",
+					},
+				},
+			},
+		},
+
+		{
+			name: "multiple",
+			str: `2e0390eb024a52963db7b95e84a9c2b12c004054a7bad9a97ec0c7c89d4681d2 hoge
+e712aff3705ac314b9a890e0ec208faa20054eee514d86ab913d768f94e01279 fuga`,
+			expected: []intoto.Subject{
+				{
+					Name: "hoge",
+					Digest: slsav02.DigestSet{
+						"sha256": "2e0390eb024a52963db7b95e84a9c2b12c004054a7bad9a97ec0c7c89d4681d2",
+					},
+				},
+				{
+					Name: "fuga",
+					Digest: slsav02.DigestSet{
+						"sha256": "e712aff3705ac314b9a890e0ec208faa20054eee514d86ab913d768f94e01279",
+					},
+				},
+			},
+		},
+		{
+			name:     "empty",
+			str:      "",
+			expected: nil,
+		},
+		{
+			name: "blank lines",
+			str: `2e0390eb024a52963db7b95e84a9c2b12c004054a7bad9a97ec0c7c89d4681d2 hoge
+
+e712aff3705ac314b9a890e0ec208faa20054eee514d86ab913d768f94e01279 fuga`,
+			expected: []intoto.Subject{
+				{
+					Name: "hoge",
+					Digest: slsav02.DigestSet{
+						"sha256": "2e0390eb024a52963db7b95e84a9c2b12c004054a7bad9a97ec0c7c89d4681d2",
+					},
+				},
+				{
+					Name: "fuga",
+					Digest: slsav02.DigestSet{
+						"sha256": "e712aff3705ac314b9a890e0ec208faa20054eee514d86ab913d768f94e01279",
+					},
+				},
+			},
+		},
+		{
+			name: "sha only",
+			str:  "2e0390eb024a52963db7b95e84a9c2b12c004054a7bad9a97ec0c7c89d4681d2",
+			err:  true,
+		},
+		{
+			name: "invalid hash",
+			str:  "abcdef hoge",
+			err:  true,
+		},
+		{
+			name: "duplicate name",
+			str: `2e0390eb024a52963db7b95e84a9c2b12c004054a7bad9a97ec0c7c89d4681d2 hoge
+2e0390eb024a52963db7b95e84a9c2b12c004054a7bad9a97ec0c7c89d4681d2 hoge`,
+			err: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if s, err := parseSubjects(tc.str); err != nil {
+				if tc.err {
+					// Error was expected.
+					return
+				}
+				t.Fatalf("unexpected error: %v", err)
+			} else {
+				if tc.err {
+					t.Fatalf("expected error but received %#v", s)
+				}
+
+				if want, got := tc.expected, s; !reflect.DeepEqual(want, got) {
+					t.Errorf("unexpected subjects, want: %#v, got: %#v", want, got)
+				}
+			}
+		})
+	}
+}
